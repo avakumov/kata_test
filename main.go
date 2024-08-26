b87ffc773e259695419e75a4a51b549300c852fa
Avakumov Vladimir <avakdev@gmail.com>
2024-08-26 18:53:40 +0300

    init

---
+++ main.go
@@ -0,0 +1,176 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func removeSpaces(text string) string {
+	res := strings.ReplaceAll(text, "\t", "")
+	res = strings.ReplaceAll(res, "\n", "")
+	return strings.ReplaceAll(res, " ", "")
+}
+
+func checkRange(number int) bool {
+	if number < 1 || number > 10 {
+		return false
+	}
+	return true
+}
+
+func isRoman(number string) bool {
+	for _, r := range number {
+		if !(r == 'I' || r == 'V' || r == 'X') {
+			return false
+		}
+	}
+
+	return true
+}
+
+func romanToInt(s string) int {
+	res := 0
+	m := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+	last := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		tmp := m[s[i]]
+		sign := 1
+		if tmp < last {
+			sign = -1
+		}
+		res += sign * tmp
+		last = tmp
+	}
+	return res
+}
+
+func intToRoman(number int) string {
+	maxRomanNumber := 3999
+	if number > maxRomanNumber {
+		return strconv.Itoa(number)
+	}
+
+	conversions := []struct {
+		value int
+		digit string
+	}{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
+	var roman strings.Builder
+	for _, conversion := range conversions {
+		for number >= conversion.value {
+			roman.WriteString(conversion.digit)
+			number -= conversion.value
+		}
+	}
+
+	return roman.String()
+}
+
+func handleOperation(number1 int, operation string, number2 int) int {
+	switch operation {
+	case "+":
+		return number1 + number2
+	case "-":
+		return number1 - number2
+	case "*":
+		return number1 * number2
+	case "/":
+		return number1 / number2
+	}
+	return 0
+}
+
+func handleCommand(str string) string {
+	text := removeSpaces(str)
+	switch text {
+	case "exit":
+		os.Exit(0)
+	case "help":
+		fmt.Println("type exit to exit")
+		fmt.Println("examples: 1 + 6, V - II")
+	default:
+		{
+			r, _ := regexp.Compile(`(\+|\-|\*|\/)`)
+			operations := r.FindAllString(text, -1)
+			if operations == nil {
+				panic("invalid operation. Use +, -, *, or /")
+			}
+			if len(operations) != 1 {
+				panic("invalid operation. Use only one operation")
+			}
+
+			values := strings.Split(text, operations[0])
+
+			//operation with roman numbers
+			if isRoman(values[0]) && isRoman(values[1]) {
+				number1 := romanToInt(values[0])
+				number2 := romanToInt(values[1])
+				result := handleOperation(number1, operations[0], number2)
+				if result < 1 {
+					panic("there are no negative Roman numbers")
+				}
+				return intToRoman(result)
+			}
+
+			number1, err := strconv.Atoi(values[0])
+			if err != nil {
+				panic("first number is not a number")
+			}
+
+			number2, err := strconv.Atoi(values[1])
+			if err != nil {
+				panic("second number is not a integer")
+			}
+
+			//check if number is in range
+			if !checkRange(number1) || !checkRange(number2) {
+				panic("number out of range")
+			}
+
+			result := handleOperation(number1, operations[0], number2)
+			return strconv.Itoa(result)
+
+		}
+	}
+	return ""
+}
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("type help to get help")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+
+		handleCommand(text)
+
+	}
+
+}
---
+++ main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCheckRange(t *testing.T) {
+	if checkRange(11) {
+		t.Errorf("checkRange(11) = true, want false")
+	}
+
+	if checkRange(-1) {
+		t.Errorf("checkRange(-1) = true, want false")
+	}
+
+}
+
+func TestHandleCommandArabic(t *testing.T) {
+	assert.Equal(t, "20", handleCommand("10 + 10"))
+	assert.Equal(t, "10", handleCommand("9 + 1"))
+	assert.Equal(t, "2", handleCommand("10 / 5"))
+	assert.Equal(t, "5", handleCommand("9 - 4"))
+	assert.Equal(t, "2", handleCommand("9 / 4"))
+	assert.Equal(t, "0", handleCommand("9 / 10"))
+	assert.Equal(t, "36", handleCommand("9 * 4"))
+	assert.Equal(t, "-9", handleCommand("1-10"))
+
+}
+
+func TestHandleCommandRoman(t *testing.T) {
+	assert.Equal(t, "V", handleCommand("X / II"))
+	assert.Equal(t, "VIII", handleCommand("X - II"))
+	assert.Equal(t, "XX", handleCommand("X * II"))
+	assert.Equal(t, "XIV", handleCommand("X + IV"))
+	assert.Equal(t, "I", handleCommand("X / X"))
+	assert.Equal(t, "I", handleCommand("III / II"))
+	assert.Equal(t, "C", handleCommand("X * X"))
+}
+
+func TestHandleCommandPanicArabic(t *testing.T) {
+	assert.Panics(t, func() { handleCommand("11+10") })
+	assert.Panics(t, func() { handleCommand("1+1+2") })
+	assert.Panics(t, func() { handleCommand("10 - 11") })
+	assert.Panics(t, func() { handleCommand("-10+10") })
+	assert.Panics(t, func() { handleCommand("0+10") })
+	assert.Panics(t, func() { handleCommand("1&2") })
+	assert.Panics(t, func() { handleCommand("1 @ 2") })
+	assert.Panics(t, func() { handleCommand("1 + 2.2") })
+	assert.Panics(t, func() { handleCommand("1 + 2 - 1") })
+}
+
+func TestHandleCommandPanicRoman(t *testing.T) {
+	assert.Panics(t, func() { handleCommand("V-VI") })
+	assert.Panics(t, func() { handleCommand("V/VI") })
+	assert.Panics(t, func() { handleCommand("V a I") })
+	assert.Panics(t, func() { handleCommand("I - I") })
+}
